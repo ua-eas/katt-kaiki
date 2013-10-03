6f2296357af7d2cc2a5c4d71f956a5df2327c576
thelg59 <thelgeland@eaglecrk.com>
2013-10-03 11:27:03 -0500

    ECE-482 Testing video capture

--- features/support/headless.rb
+++ features/support/headless.rb
@@ -24,22 +24,30 @@ unless ENV['BUILD_NUMBER'].nil?
 
   Before do
     print "\n"
-    print "Starting Video...\n"
+    print "Starting video for Jenkins...\n"
     headless.video.start_capture
   end
 
   After do |scenario|
     #if scenario.failed?
       headless.video.stop_and_save(video_path(scenario))
-      print "Stopping video...\n"
+      print "Stopping video for Jenkins...\n"
     #else
     #  headless.video.stop_and_discard
     #end
   end
 
   def video_path(scenario)
-    print "#{scenario}\n"
-    print "#{scenario.name.split.join("_")}.mov\n"
-    "#{scenario.name.split.join("_")}.mov"
+    #print "#{scenario}\n"
+    #print "#{scenario.name.split.join("_")}.mov\n"
+    #"#{scenario.name.split.join("_")}.mov"
+    basename = File.basename(scenario.file_colon_line)
+    print "#{basename}\n"
+    if basename =~ /^(.+):(\d+)$/
+      basename = "#{$1}__%04d" % $2.to_i
+    end
+    print File.join(Dir::pwd, 'features', 'videos', basename+".mov")
+    print "\n"
+    File.join(Dir::pwd, 'features', 'videos', basename+".mov")
   end
 end
