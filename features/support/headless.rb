6fcf214cb56ba921ecb34c452769eef0c6f91b83
thelg59 <thelgeland@eaglecrk.com>
2013-10-03 14:07:34 -0500

    ECE-482 Testing video capture

--- features/support/headless.rb
+++ features/support/headless.rb
@@ -26,15 +26,15 @@ unless ENV['BUILD_NUMBER'].nil?
     base_path = File.join(Dir::pwd, 'features', 'videos')
     print "\n"
     print "base_path before mk_video_dir: #{base_path}\n"
-    @video_dir = mk_video_dir(base_path)
-    print "@video_dir after mk_video_dir: #{@video_dir}\n"
+    video_dir = mk_video_dir(base_path)
+    print "video_dir after mk_video_dir: #{@video_dir}\n"
     headless.video.start_capture
   end
 
   After do |scenario|
     #if scenario.failed?
-      print "@video_dir: #{@video_dir}\n"
-      path = video_path(scenario, @video_dir)
+      print "video_dir: #{video_dir}\n"
+      path = video_path(scenario, video_dir)
       print "#{path}\n"
       headless.video.stop_and_save(path)
       print "Saved video to #{path}\n"
@@ -49,8 +49,9 @@ unless ENV['BUILD_NUMBER'].nil?
     print "video_dir in mk_video_dir: #{video_dir}\n"
     print "video_dir exist?: #{Dir::exists? video_dir}\n"
     return if Dir::exists? video_dir
-    print "returned from mk_video_dir: #{Dir::mkdir(video_dir)}"
-    Dir::mkdir(video_dir)
+    else
+      Dir::mkdir(video_dir)
+    end
   end
 
   def video_path(scenario, video_dir)
@@ -61,7 +62,7 @@ unless ENV['BUILD_NUMBER'].nil?
       basename = "#{$1}__%04d" % $2.to_i
     end
     print "video_dir in video_path: #{video_dir}\n"
-    print "#{File.join('#{video_dir}', '#{basename}.mov')}\n"
-    File.join('#{video_dir}', '#{basename}.mov')
+    print "File.join(video_dir, basename+' .mov')"
+    File.join(video_dir, basename+" .mov")
   end
 end
