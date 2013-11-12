d631b6817b46922c2ab650353a6c0e0d2d18139f
thelg59 <thelgeland@eaglecrk.com>
2013-11-12 14:28:23 -0600

    ECE-482 Testing connection to browser

--- features/support/headless.rb
+++ features/support/headless.rb
@@ -11,27 +11,27 @@
 #	  display - which desktop environment to create the display on.
 #
 # Returns nothing
-unless ENV['BUILD_NUMBER'].nil?
-  require 'headless'
+#unless ENV['BUILD_NUMBER'].nil?
+#  require 'headless'
 
   #headless = Headless.new(:display => SERVER_PORT)
-  display = ENV['BUILD_NUMBER']
-  headless = Headless.new(:display => display)
-  headless.start
+#  display = ENV['BUILD_NUMBER']
+#  headless = Headless.new(:display => display)
+#  headless.start
   
-  print "Created Headless Display-headless.rb\n"
+#  print "Created Headless Display-headless.rb\n"
 
-  at_exit do
-    headless.destroy
-  end
+#  at_exit do
+#    headless.destroy
+#  end
 
   # Public: Creates a video of the headless browser, before each scenario.
   #
   # Returns nothing
-  Before do
-    headless.video.start_capture
-    kaiki.log.debug "Starting video for Jenkins build..."
-  end
+#  Before do
+#    headless.video.start_capture
+#    kaiki.log.debug "Starting video for Jenkins build..."
+#  end
 
   # Public: Stops video recording after each scenario.
   #
@@ -39,16 +39,16 @@ unless ENV['BUILD_NUMBER'].nil?
   #   scenario - current running test.
   #
   # Returns nothing
-  After do |scenario|
+#  After do |scenario|
     #if scenario.failed?
-      path = video_path(scenario)
-      headless.video.stop_and_save(path)
-      print "Saved video file to #{path}\n"
-      kaiki.log.debug "Stopping video for Jenkins build..."
+#      path = video_path(scenario)
+#      headless.video.stop_and_save(path)
+#      print "Saved video file to #{path}\n"
+#      kaiki.log.debug "Stopping video for Jenkins build..."
     #else
       #headless.video.stop_and_discard
     #end
-  end
+#  end
 
   # Public: Defines where the video is being saved.
   #
@@ -56,11 +56,11 @@ unless ENV['BUILD_NUMBER'].nil?
   #   scenario - current running test.
   #
   # Returns file path of video
-  def video_path(scenario)
-    basename = File.basename(scenario.file_colon_line)
-    if basename =~ /^(.+):(\d+)$/
-      basename = "#{$1}__%04d" % $2.to_i
-    end
-    File.join(Dir::pwd, 'features', 'videos', basename+".mov")
-  end
-end
+#  def video_path(scenario)
+#    basename = File.basename(scenario.file_colon_line)
+#    if basename =~ /^(.+):(\d+)$/
+#      basename = "#{$1}__%04d" % $2.to_i
+#    end
+#    File.join(Dir::pwd, 'features', 'videos', basename+".mov")
+#  end
+#end
