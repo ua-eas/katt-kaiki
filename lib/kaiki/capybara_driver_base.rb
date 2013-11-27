264294b520232d0a531cf9332927d7c2b786b6ad
leahasullivan <lsullivan@eaglecrk.com>
2013-11-27 13:31:29 -0600

    ECE-482, decrease default pause time for testing

--- lib/kaiki/capybara_driver_base.rb
+++ lib/kaiki/capybara_driver_base.rb
@@ -88,7 +88,7 @@ class Kaiki::CapybaraDriver::Base
     end
 
     if ENV['BUILD_NUMBER']
-      @default_pause_time  = 5
+      @default_pause_time  = 3
     else
       @default_pause_time  = 0.5
     end
